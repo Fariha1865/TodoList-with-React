3fd3d4d8c887a0edb8d0f83d833b7165459f92ba
Fariha-Amin <fariha.amin@dsinnovators.com>
2024-05-28 17:55:49 +0600

    icons position updated

--- src/components/homePage/TaskItem.jsx
+++ src/components/homePage/TaskItem.jsx
@@ -3,8 +3,8 @@ import { RiDeleteBin5Fill, RiEdit2Fill, RiCheckFill } from "react-icons/ri";
 import { Modal } from 'react-responsive-modal';
 import 'react-responsive-modal/styles.css';
 import EditTaskForm from '../form/EditTaskForm';
-import 'react-tooltip/dist/react-tooltip.css'
-import { Tooltip } from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css';
+import { Tooltip } from 'react-tooltip';
 
 const getDaysDifference = (date1, date2) => {
   const diffTime = date2 - date1;
@@ -26,11 +26,9 @@ const TaskItem = ({ task, index, handleDeleteTask, handleEditTask, handleComplet
   const closeEditModal = () => setIsEditModalOpen(false);
 
   const confirmDeleteTask = () => {
-
     if (activeCategory === "Completed") {
       handleDeleteTask(index, activeCategory === 'Completed');
-    }
-    else {
+    } else {
       handleDeleteTask(index);
     }
     closeModal();
@@ -54,40 +52,30 @@ const TaskItem = ({ task, index, handleDeleteTask, handleEditTask, handleComplet
     <div className="mb-4 p-4 border border-gray-300 rounded-lg overflow-x-hidden" style={{ backgroundColor: task.color }}>
       <div className="flex justify-between items-center">
         <div className="flex items-center">
-          {task.emoji && <img src={task.emoji} alt="Task Emoji" />}
-          <div className='ml-5'>
-            <div className='flex justify-between gap-20'>
-              <h3 className="text-lg font-bold">{task.taskName}</h3>
-              <span className="text-gray-500">
-                <span className='font-bold text-black mr-2 text-sm'>Deadline:</span> {deadlineDate.toLocaleString()}
-              </span>
-            </div>
+          {task.emoji && <img src={task.emoji} alt="Task Emoji" className="mr-4" />}
+          <div>
+            <h3 className="text-lg font-bold">{task.taskName}</h3>
             <p className="text-gray-700 mt-2">{task.description}</p>
-            {daysDifference !== 0 && (
-              <p className={`mt-2 ${daysDifference < 0 ? 'text-red-500' : 'text-green-700'}`}>{deadlineText}</p>
-            )}
-            <div className="mt-2 flex justify-between items-center">
-              <span className={`rounded ${task.color}`}>
+            <p className={`mt-2 ${daysDifference < 0 ? 'text-red-500' : 'text-green-700'}`}>{deadlineText}</p>
+            <div className="mt-2 flex items-center">
+              <span className={`rounded ${task.color} mr-4`}>
                 <span className='font-bold text-black mr-2 text-sm'>Category:</span> {task.category}
               </span>
-              <span className="px-2 py-1 rounded bg-gray-200 ml-3">{task.priority == 1 ? "High" : task.priority == 2 ? "Medium" : "Low"} priority</span>
-              <div className="flex space-x-2">
-                {activeCategory !== 'Completed' && (
-                  <>
-                    <RiEdit2Fill className='text-2xl cursor-pointer ml-3' onClick={openEditModal} data-tooltip-id="edit-tooltip" data-tooltip-content="Edit Task"/>
-                    <RiCheckFill className='text-2xl cursor-pointer' onClick={() => handleCompleteTask(index)} data-tooltip-id="complete-tooltip" data-tooltip-content="Complete Task"/>
-                  </>
-                )}
-             
-                  <RiDeleteBin5Fill className='text-2xl cursor-pointer' onClick={openModal} data-tooltip-id="delete-tooltip" data-tooltip-content="Delete Task"/>
-                  <Tooltip id="delete-tooltip" />
-                  <Tooltip id="edit-tooltip" />
-                  <Tooltip id="complete-tooltip" />
-                
-
-              </div>
+              <span className="px-2 py-1 rounded bg-gray-200">{task.priority == 1 ? "High" : task.priority == 2 ? "Medium" : "Low"} priority</span>
             </div>
           </div>
+        </div>
+        <div className="flex gap-2 items-center ml-10">
+          {activeCategory !== 'Completed' && (
+            <>
+              <RiEdit2Fill className='text-2xl cursor-pointer' onClick={openEditModal} data-tooltip-id="edit-tooltip" data-tooltip-content="Edit Task" />
+              <RiCheckFill className='text-2xl cursor-pointer' onClick={() => handleCompleteTask(index)} data-tooltip-id="complete-tooltip" data-tooltip-content="Complete Task" />
+            </>
+          )}
+          <RiDeleteBin5Fill className='text-2xl cursor-pointer' onClick={openModal} data-tooltip-id="delete-tooltip" data-tooltip-content="Delete Task" />
+          <Tooltip id="delete-tooltip" />
+          <Tooltip id="edit-tooltip" />
+          <Tooltip id="complete-tooltip" />
         </div>
       </div>
       <Modal open={isModalOpen} onClose={closeModal} center>
